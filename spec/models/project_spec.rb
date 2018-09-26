bd1433e450b001b929878de3091eb58a8acf0daf
Alex Kalinkou <glip.fl@gmail.com>
2018-09-26 10:20:15 -0400

    Initial commit

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec_helper'
+
+RSpec.describe Project do
+
+  it 'considers a project with no tasks to be done' do
+
+    project = Project.new
+
+    expect(project.done?).to be_truthy
+  end
+end
