54fc42f0ce30e57c9958b4acc555a73d6bf13d98
Alex Kalinkou <glip.fl@gmail.com>
2018-10-15 16:17:42 -0400

    The Days Are Action-Packed Chapter in progress - Refactoring to Single-Assertion Specs

--- spec/workflows/creates_project_spec.rb
+++ spec/workflows/creates_project_spec.rb
@@ -2,9 +2,77 @@ require 'rails_helper'
 
 describe CreatesProject do
 
-  it 'should create a project given a name' do
-    creator = CreatesProject.new(name: 'Project Runway')
-    creator.build
-    expect( creator.project.name ).to eq( 'Project Runway' )
+  let(:creator) { CreatesProject.new( name: 'Project Runway', task_string: task_string )}
+
+  describe 'initialization' do
+    let( :task_string ) { '' }
+    it 'creates a project given a name' do
+      creator.build
+      expect(creator.project.name).to eq('Project Runway')
+    end
+  end
+
+  describe 'task string parsing' do
+    let(:tasks) { creator.convert_string_to_tasks }
+
+    it 'with an empty string' do
+      let(:task_string) { '' }
+      specify { expect( tasks ).to be_empty }
+    end
+
+    it 'with a single string' do
+      creator = CreatesProject.new(name: 'Project Runway', task_string: 'Start Things')
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.size).to eq(1)
+      expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)
+    end
+
+    it 'handles a single string with size' do
+      creator = CreatesProject.new(name: 'Project Runway', task_string: 'Start Things:3')
+      tasks = creator. convert_string_to_tasks
+      expect(tasks.size).to eq(1)
+      expect(tasks.first).to have_attributes(title: 'Start Things', size: 3)
+    end
+
+    it 'handles a single string with size zero' do
+      creator = CreatesProject.new( name: 'Project Runway', task_string: 'Start Things:0')
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.size).to eq(1)
+      expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)
+    end
+
+    it 'handles a single string with malformed size' do
+      creator = CreatesProject.new( name: 'Project Runway', task_string: 'Start Things:')
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.size).to eq(1)
+      expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)
+    end
+
+    it 'handles a single string with negative size' do
+      creator = CreatesProject.new( name: 'Project Runway', task_string: 'Start Things:-1')
+      tasks = creator.convert_string_to_tasks
+      expect(tasks.size).to eq(1)
+      expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)
+    end
+
+    it 'handles multiple tasks' do
+      creator = CreatesProject.new( name: 'Project Runway', task_string: 'Start Things:3\nEndThings:2')
+      tasks = creator.convert_sting_to_tasks
+      expect(tasks.size).to eq(2)
+      expect(tasks).to match(
+                           [
+                               an_object_having_attributes(title: 'Start Things', size: 3),
+                               an_object_having_attributes(title: 'End Things', size: 2)
+                           ]
+                       )
+    end
+
+    it 'attaches tasks to the project' do
+      creator = CreatesProject.new( name: 'Project Runway', task_string: 'Start Things:3\nEnd Things:2')
+      creator.create
+      expect(creator.project.tasks.size).to eq(2)
+      expect(creator.project).not_to be_a_new_record
+    end
   end
 end
+
