56446111b48f127875311699d66bd1d383e93422
Alex Kalinkou <glip.fl@gmail.com>
2018-10-16 10:30:40 -0400

    Test-Driven Rails - The Days Are Action-Packed - Done

--- spec/workflows/creates_project_spec.rb
+++ spec/workflows/creates_project_spec.rb
@@ -15,63 +15,57 @@ describe CreatesProject do
   describe 'task string parsing' do
     let(:tasks) { creator.convert_string_to_tasks }
 
-    it 'with an empty string' do
+    describe 'with an empty string' do
       let(:task_string) { '' }
       specify { expect( tasks ).to be_empty }
     end
 
-    it 'with a single string' do
-      creator = CreatesProject.new(name: 'Project Runway', task_string: 'Start Things')
-      tasks = creator.convert_string_to_tasks
-      expect(tasks.size).to eq(1)
-      expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)
+    describe 'with a single string' do
+      let(:task_string) { 'Start Things' }
+      specify { expect( tasks.size ).to eq(1) }
+      specify { expect( tasks.first ).to have_attributes( title: 'Start Things', size: 1 )}
     end
 
-    it 'handles a single string with size' do
-      creator = CreatesProject.new(name: 'Project Runway', task_string: 'Start Things:3')
-      tasks = creator. convert_string_to_tasks
-      expect(tasks.size).to eq(1)
-      expect(tasks.first).to have_attributes(title: 'Start Things', size: 3)
+    describe 'with a single string with size' do
+      let( :task_string ) { 'Start Things:3'}
+      specify { expect(tasks.size).to eq(1) }
+      specify { expect(tasks.first).to have_attributes(title: 'Start Things', size: 3) }
     end
 
-    it 'handles a single string with size zero' do
-      creator = CreatesProject.new( name: 'Project Runway', task_string: 'Start Things:0')
-      tasks = creator.convert_string_to_tasks
-      expect(tasks.size).to eq(1)
-      expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)
+    describe 'with a single string with size zero' do
+      let( :task_string ) { 'Start Things:0' }
+      specify { expect(tasks.size).to eq(1) }
+      specify {expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)}
     end
 
-    it 'handles a single string with malformed size' do
-      creator = CreatesProject.new( name: 'Project Runway', task_string: 'Start Things:')
-      tasks = creator.convert_string_to_tasks
-      expect(tasks.size).to eq(1)
-      expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)
+    describe 'with a single string with malformed size' do
+      let( :task_string ) { 'Start Things:' }
+      specify {expect(tasks.size).to eq(1)}
+      specify {expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)}
     end
 
-    it 'handles a single string with negative size' do
-      creator = CreatesProject.new( name: 'Project Runway', task_string: 'Start Things:-1')
-      tasks = creator.convert_string_to_tasks
-      expect(tasks.size).to eq(1)
-      expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)
+    describe 'with a single string with negative size' do
+      let ( :task_string ) { 'Start Things:-1' }
+      specify { expect(tasks.size).to eq(1) }
+      specify {expect(tasks.first).to have_attributes(title: 'Start Things', size: 1)}
     end
 
-    it 'handles multiple tasks' do
-      creator = CreatesProject.new( name: 'Project Runway', task_string: 'Start Things:3\nEndThings:2')
-      tasks = creator.convert_sting_to_tasks
-      expect(tasks.size).to eq(2)
-      expect(tasks).to match(
+    describe 'handles multiple tasks' do
+      let( :task_string ) { 'Start Things:3\nEnd Things:2' }
+      specify { expect(tasks.size).to eq(2) }
+      specify {expect(tasks).to match(
                            [
                                an_object_having_attributes(title: 'Start Things', size: 3),
                                an_object_having_attributes(title: 'End Things', size: 2)
                            ]
-                       )
+                       )}
     end
 
-    it 'attaches tasks to the project' do
-      creator = CreatesProject.new( name: 'Project Runway', task_string: 'Start Things:3\nEnd Things:2')
-      creator.create
-      expect(creator.project.tasks.size).to eq(2)
-      expect(creator.project).not_to be_a_new_record
+    describe 'attaches tasks to the project' do
+      let( :task_string ) { 'Start Things:3\nEnd Things:2' }
+      before ( :example ) { creator.create }
+      specify {expect(creator.project.tasks.size).to eq(2)}
+      specify {expect(creator.project).not_to be_a_new_record}
     end
   end
 end
