3b0b0f85c43ce9378f879fc7d1326825eff384c9
Alex Kalinkou <glip.fl@gmail.com>
2019-03-18 13:24:52 -0400

    Fix issues found by factory bot lint

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 class Task < ApplicationRecord
   include Sizeable
 
--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
   factory :project, class: 'Project' do
     name { 'Project Runaway' }
     due_date { 1.week.from_now }
+
+    trait :soon do
+      due_date { 1.day.from_now }
+    end
+
+    trait :later do
+      due_date { 1.month.from_now }
+    end
   end
 end
