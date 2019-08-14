d000a0b47011a5f1367539486464b1a5d402edf4
Alex Kalinkou <glip.fl@gmail.com>
2019-08-14 17:12:49 -0400

    Add specs

---
+++ spec/helpers/projects_helper_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe ProjectsHelper, type: :helper do
+  let(:project) { Project.new(name: 'Project Runway') }
+
+  it 'should augment with status info when on schedule' do
+    allow(project).to receive(:on_schedule?).and_return(true)
+    actual = helper.name_with_status(project)
+    expect(actual).to have_selector('span.on_schedule', text: 'Project Runway')
+  end
+
+  it 'should augment with status info when behind schedule' do
+    allow(project).to receive(:on_schedule?).and_return(false)
+    actual = helper.name_with_status(project)
+    expect(actual).to have_selector('span.behind_schedule', text: 'Project Runway')
+  end
+end
