4595e0227571a1fb9cc35dc9f6f0253f353ce231
Alex Kalinkou <glip.fl@gmail.com>
2019-08-14 14:46:43 -0400

    Work is not complete

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,5 +1,9 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
 const environment = require('./environment')
+
+// process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+environment.plugins.get('Manifest').options.writeToFileEmit = process.env.NODE_ENV !== 'test'
+
+const config = environment.toWebpackConfig()
+config.devtool = 'inline-source-map'
 
 module.exports = environment.toWebpackConfig()
